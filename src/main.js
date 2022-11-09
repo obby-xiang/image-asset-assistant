6531087a951e42164de4a99abf583690f2db71f0
obby-xiang <liuxiang.obby@qq.com>
2022-11-09 23:22:32 +0800

    主页简单实现

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import ElementPlus from 'element-plus';
+import zhCN from 'element-plus/es/locale/lang/zh-cn';
 
 import App from './App.vue';
 import router from './router';
@@ -9,4 +10,8 @@ import './assets/index.css';
 
 const app = createApp(App);
 
-app.use(createPinia()).use(router).use(ElementPlus).mount('#app');
+app
+  .use(createPinia())
+  .use(router)
+  .use(ElementPlus, { locale: zhCN })
+  .mount('#app');
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,20 +1,59 @@
 <template>
-  <h1>Home</h1>
-  <div v-for="item in iconData.icons" :key="item.id">
-    <img
-      :src="`data:image/svg+xml;base64,${Base64.encode(item.svgCode)}`"
-      height="36"
-      width="36"
-    />
-    <div>{{ JSON.stringify(item) }}</div>
-  </div>
+  <el-container class="tw-container tw-mx-auto tw-px-4">
+    <el-main class="tw-mx-auto tw-max-w-screen-xl">
+      <el-input :prefix-icon="SearchIcon" placeholder="搜索" size="large">
+        <template #prepend>
+          <el-select
+            v-model="iconCategory"
+            class="tw-w-32"
+            clearable
+            placeholder="类别"
+            size="large"
+          >
+            <el-option
+              v-for="item in iconData.summary?.moduleCn ?? []"
+              :key="item.module"
+              :label="item.module"
+              :value="item.module"
+            />
+          </el-select>
+        </template>
+      </el-input>
+
+      <div class="tw-flex tw-flex-wrap tw-gap-y-2 tw-my-4">
+        <el-button
+          v-for="item in iconData.summary?.styleCn ?? []"
+          :key="item"
+          :type="iconStyle === item ? 'primary' : undefined"
+          round
+          @click="iconStyle = iconStyle === item ? undefined : item"
+        >
+          {{ item }}
+        </el-button>
+      </div>
+
+      <div class="tw-grid tw-grid-cols-4 tw-justify-items-center">
+        <div v-for="item in iconData.icons" :key="item.id" class="">
+          <img
+            :alt="item.nameCn"
+            :src="`data:image/svg+xml;base64,${Base64.encode(item.svgCode)}`"
+            height="40"
+            width="40"
+          />
+        </div>
+      </div>
+    </el-main>
+  </el-container>
 </template>
 
 <script setup>
-import { shallowRef } from 'vue';
+import { ref, shallowRef } from 'vue';
+import { Search as SearchIcon } from '@element-plus/icons-vue';
 import { Base64 } from 'js-base64';
 
 const iconData = shallowRef({});
+const iconCategory = ref();
+const iconStyle = ref();
 
 window.network.fetchIconData().then((data) => (iconData.value = data));
 </script>
